ef8ca0674caf8ed52aec397e66a0d9647b47a55e
mertskv <kmerts2@yandex.ru>
2025-09-10 20:55:53 +0500

    Task4

---
+++ Tyuiu.MertsKV.Sprint0.Task4.V0.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.MertsKV.Sprint0.Task4.V0.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.MertsKV.Sprint0.Task4.V0.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("83953332-b605-434b-b4ad-8ac7f636249f")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Tyuiu.MertsKV.Sprint0.Task4.V0/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Tyuiu.MertsKV.Sprint0.Task4.V0.Lib;
+namespace Tyuiu.MertsKV.Sprint0.Task4.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Пример линейной структуры программы 
+            // Вызов методов сложения Addition 
+            Console.WriteLine(DataServise.Addition(1, 5));
+
+            Console.WriteLine(DataServise.Subtraction(15, 5));
+
+            Console.WriteLine(DataServise.Multiplication(10, 10));
+
+            Console.WriteLine(DataServise.Division(5, 5));
+
+            Console.ReadKey();
+
+        }
+    }
+}
